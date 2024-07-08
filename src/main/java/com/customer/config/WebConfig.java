e9eb1ea8f52a5c58ff95b1bd9503f7e901e3def7
gusperales06 <121507623+gusperales06@users.noreply.github.com>
2024-07-08 15:10:07 -0600

    fix: Changed the allowed methods by their corresponding constants

--- src/main/java/com/customer/config/WebConfig.java
+++ src/main/java/com/customer/config/WebConfig.java
@@ -7,6 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.http.HttpHeaders.ACCEPT;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 @Configuration
 public class WebConfig {
@@ -17,7 +22,7 @@ public class WebConfig {
             public void addCorsMappings(final CorsRegistry registry) {
                 registry.addMapping("/customer-car/**")
                         .allowedOrigins("*")
-                        .allowedMethods()
+                        .allowedMethods(GET.toString(), HEAD.toString(), PUT.toString(), POST.toString(), DELETE.toString())
                         .allowedHeaders(CONTENT_TYPE, ACCEPT);
             }
         };
