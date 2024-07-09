2a2ca4d6b038a73d5cb5675ce0bebd838958a43a
gusperales06 <121507623+gusperales06@users.noreply.github.com>
2024-07-09 17:19:19 -0600

    fix: Null check to the Customer Info DTO

--- src/main/java/com/customer/model/converter/CustomerConverter.java
+++ src/main/java/com/customer/model/converter/CustomerConverter.java
@@ -5,13 +5,32 @@ import com.customer.model.entity.Customers;
 import org.springframework.core.convert.converter.Converter;
 
 public class CustomerConverter implements Converter<CustomerDTO, Customers> {
+    private Integer customerId;
+    private Customers customer;
+
+    public CustomerConverter() {
+    }
+
+    public CustomerConverter(final Integer customerId) {
+        this.customerId = customerId;
+    }
+
     @Override
     public Customers convert(CustomerDTO source) {
-        var customer = new Customers(source.getFirstName(), source.getLastName(), source.getBirthdate());
-        customer.setGender(source.getCustomersInfo().getGender());
-        customer.setHouseholdIncome(source.getCustomersInfo().getHouseholdIncome());
-        customer.setPhoneNumber(source.getCustomersInfo().getPhoneNumber());
-        customer.setEmail(source.getCustomersInfo().getEmail());
+        customer = new Customers(source.getFirstName(), source.getLastName(), source.getBirthdate());
+        if (customerId != null) {
+            customer.setCustomerId(this.customerId);
+        }
+        mapCustomerAdditionalInfo(source);
         return customer;
+    }
+
+    private void mapCustomerAdditionalInfo(CustomerDTO source) {
+        if (source.getCustomersInfo() != null) {
+            customer.setGender(source.getCustomersInfo().getGender());
+            customer.setHouseholdIncome(source.getCustomersInfo().getHouseholdIncome());
+            customer.setPhoneNumber(source.getCustomersInfo().getPhoneNumber());
+            customer.setEmail(source.getCustomersInfo().getEmail());
+        }
     }
 }
